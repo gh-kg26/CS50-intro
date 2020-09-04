88689666511d247d5280438d75c4ecec8708a955
guptakg2620 <guptakg2620@users.noreply.github.com>
2020-09-04 11:05:53 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 
-void disp(char c,int x);
+void disp(char c, int x);
 
 int main(void)
 {
@@ -10,23 +10,23 @@ int main(void)
     {
         height = get_int("enter the height b/w 1 to 8:");
     }
-    while(height<1||height>8);
+    while (height < 1 || height > 8);
 
-    for (int i=0;i<height;i++)
+    for (int i = 0; i < height; i++)
     {
-        disp(' ',height-1-i);
-        disp('#',i+1);
-        disp(' ',2);
-        disp('#',i+1);
+        disp(' ', height - 1 - i);
+        disp('#', i + 1);
+        disp(' ', 2);
+        disp('#', i + 1);
         printf("\n");
     }
 }
-void disp(char c,int x)
+void disp(char c, int x)
 {
 
-    for(int i=0;i<x;i++)
+    for (int i = 0; i < x; i++)
     {
-        printf("%c",c);
+        printf("%c", c);
 
     }
 }
