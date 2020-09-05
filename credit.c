931a4740f9b15a4539dd113b6117153344892e8c
guptakg2620 <guptakg2620@users.noreply.github.com>
2020-09-05 10:31:42 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -1,67 +1,72 @@
 #include <stdio.h>
 #include <cs50.h>
 
-void check_validity( long v, long w);
+void check_validity(long v, long w);
 
-void check_cardtype( long ct);
+void check_cardtype(long ct);
 
-int length( long l);
+int length(long l);
 
 int main(void)
 {
- long Number;
- do
- {
-    Number = get_long("Number:");
+    long Number;
+    do
+    {
 
- } while(Number < 0);
+        Number = get_long("Number:");
 
- length(Number);
+    }
+    while (Number < 0);
 
- int len = length(Number);
+    length(Number);
 
-if( len == 13 || len == 15 || len == 16)
-{
+    int len = length(Number);
 
- check_validity(Number,Number);
+    if (len == 13 || len == 15 || len == 16)
+    {
 
-} else
-{
-    printf("INVALID\n");
-}
+        check_validity(Number, Number);
+    }
+
+    else
+    {
+        printf("INVALID\n");
+    }
 }
 int length(long l)
 {
     int lt = 0;
 
-    for( lt = 0; l != 0; l /= 10)
+    for (lt = 0; l != 0; l /= 10)
     {
-    lt++;
+        lt++;
     }
     return lt;
 }
-void check_validity (long v, long w)
+void check_validity(long v, long w)
 {
     int val;
     int tot = 0;
     int mult;
-    for(val = 0; v != 0; val++, v /= 10)
+    for (val = 0; v != 0; val++, v /= 10)
     {
-        if(val % 2 == 0)
+        if (val % 2 == 0)
         {
             tot = tot + v % 10;
-        }else
+        }
+        else
         {
-            mult = 2*(v % 10);
-            tot = tot + mult/10 + mult%10;
+            mult = 2 * (v % 10);
+            tot = tot + mult / 10 + mult % 10;
         }
 
 
     }
-    if((tot % 10) == 0)
+    if ((tot % 10) == 0)
     {
         check_cardtype(w);
-    }else
+    }
+    else
     {
         printf("INVALID\n");
     }
@@ -72,15 +77,15 @@ void check_validity (long v, long w)
 void check_cardtype(long ct)
 {
 
-   if(((ct >= 34e13 && ct < 35e13) || (ct >= 37e13 && ct  <38e13)))
+    if (((ct >= 34e13 && ct < 35e13) || (ct >= 37e13 && ct < 38e13)))
     {
         printf("AMEX\n");
     }
-    else if(((ct >= 4e12 && ct < 5e12) || (ct >= 4e15 && ct < 5e15)))
+    else if (((ct >= 4e12 && ct < 5e12) || (ct >= 4e15 && ct < 5e15)))
     {
         printf("VISA\n");
     }
-     else if(((ct >= 51e14) && (ct < 56e14)))
+    else if (((ct >= 51e14) && (ct < 56e14)))
     {
         printf("MASTERCARD\n");
     }
