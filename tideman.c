8a517906a72be1e427028acdb8746cfe3749d261
guptakg2620 <guptakg2620@users.noreply.github.com>
2020-09-27 16:55:35 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -227,10 +227,10 @@ bool check_cycl(int winner, int loser)
 // Print the winner of the election
 void print_winner(void)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < MAX; i++)
     {
         bool src = true;
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = 0; j < MAX; j++)
         {
             if (locked[i][j] == true)
             {
